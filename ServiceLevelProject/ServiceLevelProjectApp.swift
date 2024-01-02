46ad422102373ff1ebf4b20f8da3950a5b51079f
Kim junhwan <tazanboy1@naver.com>
2024-01-02 13:53:59 +0900

    프로젝트 생성

---
+++ ServiceLevelProject/ServiceLevelProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceLevelProjectApp.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/02.
+//
+
+import SwiftUI
+
+@main
+struct ServiceLevelProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
