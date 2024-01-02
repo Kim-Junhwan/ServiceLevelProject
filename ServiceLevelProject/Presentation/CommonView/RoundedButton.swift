8c8187d7b4d760639950fd5f0c962d0520b3b777
Kim junhwan <tazanboy1@naver.com>
2024-01-02 23:06:46 +0900

    [feat] 기본 버튼 구현

---
+++ ServiceLevelProject/Presentation/CommonView/RoundedButton.swift
@@ -0,0 +1,37 @@
+//
+//  GreenButton.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/02.
+//
+
+import SwiftUI
+
+struct RoundedButton<Label: View>: View {
+    
+    let action: () -> Void
+    let label: () -> Label
+    var backgroundColor: Color
+    
+    var body: some View {
+        Button(action: action) {
+            label()
+                .frame(maxWidth: .infinity)
+                .padding([.top, .bottom], 12)
+                .padding([.leading, .trailing], 16)
+                .foregroundStyle(.white)
+                .background(backgroundColor)
+                .clipShape(.rect(cornerRadius: 8))
+                .font(CustomFont.title2.font)
+        }
+    }
+}
+
+#Preview {
+    RoundedButton(action: {
+        
+    }, label: {
+        Text("123")
+    }, backgroundColor: .brandGreen)
+
+}
