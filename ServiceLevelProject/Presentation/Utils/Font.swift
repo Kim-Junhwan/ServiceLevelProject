d06ad6aa73af87ad2a9e5dcf34ac5bfb786b3db2
Kim junhwan <tazanboy1@naver.com>
2024-01-02 15:57:44 +0900

    [feat] Font 정의

---
+++ ServiceLevelProject/Presentation/Utils/Font.swift
@@ -0,0 +1,32 @@
+//
+//  Font.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/02.
+//
+
+import Foundation
+import SwiftUI
+
+enum CustomFont {
+    case title1
+    case title2
+    case bodyBold
+    case body
+    case caption
+    
+    var font: Font {
+        switch self {
+        case .title1:
+            return .system(size: 22, weight: .bold)
+        case .title2:
+            return .system(size: 14, weight: .bold)
+        case .bodyBold:
+            return .system(size: 13, weight: .bold)
+        case .body:
+            return .system(size: 13, weight: .regular)
+        case .caption:
+            return .system(size: 12, weight: .regular)
+        }
+    }
+}
