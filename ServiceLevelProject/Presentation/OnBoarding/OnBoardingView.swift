fc1973fb3754e39c41767f2c1932361329322c2b
Kim junhwan <tazanboy1@naver.com>
2024-01-02 21:31:06 +0900

    [feat] SplashView 문구 크기 지정

---
+++ ServiceLevelProject/Presentation/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/02.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
--- ServiceLevelProject/Presentation/SplashScreen/SplashScreenView.swift
+++ ServiceLevelProject/Presentation/SplashScreen/SplashScreenView.swift
@@ -10,19 +10,17 @@ import SwiftUI
 struct SplashScreenView: View {
     var body: some View {
         ZStack {
-            VStack {
+            GeometryReader(content: { geometry in
                 Text("새싹톡을 사용하면 어디서나 팀을 모을 수 있습니다")
                     .multilineTextAlignment(.center)
-                    .padding(.leading)
-                    .padding(.trailing)
                     .offset(y: 39)
+                    .position(x: geometry.size.width/2, y: 39.0)
+                    .frame(width: geometry.size.width*0.8)
                     .font(CustomFont.title1.font)
-                Spacer()
-            }
+            })
             Image("onboarding")
                 .frame(width: 368, height: 368)
                 .aspectRatio(CGSize(width: 1, height: 1), contentMode: .fill)
-            
         }
     }
 }
