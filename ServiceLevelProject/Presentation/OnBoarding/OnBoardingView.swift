4e17276ac1e6b8a0869bcde49a3526a0226349f8
Kim junhwan <tazanboy1@naver.com>
2024-01-02 23:06:59 +0900

    [feat] 온보딩 화면 구현

--- ServiceLevelProject/Presentation/OnBoarding/OnBoardingView.swift
+++ ServiceLevelProject/Presentation/OnBoarding/OnBoardingView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct OnBoardingView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            SplashScreenView()
+            VStack {
+                Spacer()
+                RoundedButton(action: {
+                    
+                }, label: {
+                    Text("시작하기")
+                }, backgroundColor: .brandGreen )
+                .padding(24)
+
+            }
+        }
     }
 }
 
