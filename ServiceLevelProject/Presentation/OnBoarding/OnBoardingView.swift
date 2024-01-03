1a47659224db2472e7540cf488187d774ce762e7
Kim junhwan <tazanboy1@naver.com>
2024-01-03 19:31:30 +0900

    [feat] 온보딩 화면 시작버튼 누를 시 소셜로그인 화면 나오는 기능 추가

--- ServiceLevelProject/Presentation/OnBoarding/OnBoardingView.swift
+++ ServiceLevelProject/Presentation/OnBoarding/OnBoardingView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 
 struct OnBoardingView: View {
     
+    @State var showRegisterView: Bool = false
+    
     var body: some View {
         ZStack {
             SplashScreenView()
             VStack {
                 Spacer()
                 RoundedButton(action: {
-                    
+                    showRegisterView.toggle()
                 }, label: {
                     Text("시작하기")
                 }, backgroundColor: .brandGreen )
                 .padding(24)
-
             }
         }
+        .background(.backgroundPrimary)
+        .sheet(isPresented: $showRegisterView, content: {
+            SocialLoginView()
+        })
     }
 }
 
