16f14420155461a5bb7772f9bade4cc9f51b98d1
Kim junhwan <tazanboy1@naver.com>
2024-01-03 19:30:59 +0900

    [feat] 소셜 로그인 화면 구현

---
+++ ServiceLevelProject/Presentation/OnBoarding/SocialLoginView.swift
@@ -0,0 +1,71 @@
+//
+//  SocialLoginView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/03.
+//
+
+import Foundation
+import SwiftUI
+
+struct SocialLoginView: View {
+    
+    var body: some View {
+        VStack (spacing: 16) {
+            RoundedButton(action: {
+                
+            }, label: {
+                HStack (spacing: 5) {
+                    Image(.appleLogo)
+                        .resizable()
+                        .scaledToFit()
+                    Text("Apple로 계속하기")
+                }
+            }, backgroundColor: .appleBlack)
+            .frame(height: 44)
+            
+            RoundedButton(action: {
+                
+            }, label: {
+                HStack (spacing: 8) {
+                    Image(.kakaoLogo)
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    
+                    Text("카카오톡으로 계속하기")
+                        .foregroundStyle(.black)
+                }
+            }, backgroundColor: .kakaoYellow)
+            
+            RoundedButton(action: {
+                
+            }, label: {
+                HStack (spacing: 4) {
+                    Image(.email)
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    
+                    Text("이메일로 계속하기")
+                }
+            }, backgroundColor: .brandGreen)
+            
+            HStack(spacing: 0) {
+                Text("또는")
+                Button(" 새롭게 회원가입 하기") {
+                    
+                }
+                .foregroundStyle(.brandGreen)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding([.leading, .trailing], 35)
+        .background(.backgroundPrimary)
+        .presentationDetents([.height(290)])
+        .presentationDragIndicator(.visible)
+        .font(CustomFont.title2.font)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
