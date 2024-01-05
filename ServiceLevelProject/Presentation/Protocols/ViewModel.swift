2e1d0025b7fb67c6abf0bf36986ac142ba420183
Kim junhwan <tazanboy1@naver.com>
2024-01-05 21:19:27 +0900

    [feat] ViewModel 정의

--- ServiceLevelProject/Presentation/Protocols/ViewModel.swift
+++ ServiceLevelProject/Presentation/Protocols/ViewModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
+
+protocol ViewModel: ObservableObject {
+    associatedtype State
+    associatedtype Input
+    
+    var state: State { get }
+    func trigger(_ input: Input)
+}
