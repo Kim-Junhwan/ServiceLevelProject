393222db40f305a165ecd8fc63630cfe7511784e
Kim junhwan <tazanboy1@naver.com>
2024-01-06 23:28:39 +0900

    [feat] 카카오 로그인 기능 구현

---
+++ ServiceLevelProject/Data/SocialLogin/KakaoLoginManager.swift
@@ -0,0 +1,57 @@
+//
+//  KakaoLoginManager.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/06.
+//
+
+import Foundation
+import KakaoSDKUser
+import KakaoSDKCommon
+
+enum KakaoLoginError: Error {
+    case cannotFetchToken
+}
+
+struct KakaoLoginManager {
+    
+    typealias token = String
+    
+    private func loginWithKakaoTalk() async throws -> token {
+        return try await withCheckedThrowingContinuation { continuation in
+            UserApi.shared.loginWithKakaoTalk { token, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                }
+                guard let oauthToken = token?.accessToken else {
+                    continuation.resume(throwing: KakaoLoginError.cannotFetchToken)
+                    return
+                }
+                continuation.resume(returning: oauthToken)
+            }
+        }
+    }
+    
+    private func loginWithKakaoAccount() async throws -> token {
+        return try await withCheckedThrowingContinuation { continuation in
+            UserApi.shared.loginWithKakaoAccount { token, error in
+                if let error {
+                    continuation.resume(throwing: error)
+                }
+                guard let oauthToken = token?.accessToken else {
+                    continuation.resume(throwing: KakaoLoginError.cannotFetchToken)
+                    return
+                }
+                continuation.resume(returning: oauthToken)
+            }
+        }
+    }
+    
+    func login() async throws -> token {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            return try await loginWithKakaoTalk()
+        } else {
+            return try await loginWithKakaoAccount()
+        }
+    }
+}
