342a6356e38bf955406fda1102e9689fbff0330a
Kim junhwan <tazanboy1@naver.com>
2024-01-07 23:45:07 +0900

    [feat] 유효성 검사 로직 구현

---
+++ ServiceLevelProject/Global/Validator/Validator.swift
@@ -0,0 +1,35 @@
+//
+//  EmailValidator.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/07.
+//
+
+import Foundation
+
+struct Validator {
+    
+    enum ValidateCase {
+        case email
+        case nick
+        case phoneNumber
+        case password
+        
+        var regex: String {
+            switch self {
+            case .email:
+                return "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.com$"
+            case .nick:
+                return "[A-Z0-9a-z가-힣]{1,30}"
+            case .phoneNumber:
+                return "^01[0-9]-([0-9]{3,4})-([0-9]{4})$"
+            case .password:
+                return "^(?=.*[A-Z])(?=.*[a-z])(?=.*[~!@#$%^&*])(?=.*[0-9]).{8,}"
+            }
+        }
+    }
+    
+    static func isValid(category: ValidateCase, _ value: String) -> Bool {
+        return value.range(of: category.regex, options: .regularExpression) != nil
+    }
+}
