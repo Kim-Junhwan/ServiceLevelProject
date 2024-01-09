79c3f236855cf2df498f4710f14a8fec7fba8984
Kim junhwan <tazanboy1@naver.com>
2024-01-09 13:50:43 +0900

    [feat] 회원가입화면 가입버튼 누를시 focus되는 텍스트 필드로 스크롤 기능 추가

--- ServiceLevelProject/Presentation/Register/RegisterView.swift
+++ ServiceLevelProject/Presentation/Register/RegisterView.swift
@@ -15,57 +15,64 @@ struct RegisterView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                ScrollView {
-                    VStack (spacing: 24) {
-                        TitleTextField(title: "이메일", isValid: $viewModel.state.isValidEmail, placeHolder: "이메일을 입력하세요", kind: .textField, textFieldTitle: $viewModel.email, splitView: {
-                            RoundedButton(action: {
-                                
-                            }, label: {
-                                Text("중복확인")
-                            }, backgroundColor: !viewModel.email.isEmpty ? .brandGreen : .brandInactive)
-                            .frame(width: 100)
-                            .disabled(viewModel.email.isEmpty)
-                        })
-                        .focused($focusFeild, equals: .email)
-                        
-                        TitleTextField(title: "닉네임", isValid: $viewModel.state.isValidNick, placeHolder: "닉네임을 입력하세요", kind: .textField, textFieldTitle: $viewModel.nick)
-                            .focused($focusFeild, equals: .nick)
-                        
-                        TitleTextField(title: "연락처", isValid: $viewModel.state.isValidPhoneNumber, placeHolder: "전화번호를 입력하세요", kind: .textField, textFieldTitle: $viewModel.phoneNumber)
-                            .focused($focusFeild, equals: .phoneNumber)
-                        
-                        TitleTextField(title: "비밀번호", isValid: $viewModel.state.isValidPassword, placeHolder: "비밀번호을 입력하세요", kind: .secureField, textFieldTitle: $viewModel.password)
-                            .focused($focusFeild, equals: .password)
-                        
-                        TitleTextField(title: "비밀번호 확인", isValid: $viewModel.state.passwordIsSame, placeHolder: "비밀번호를 한번 더 입력하세요", kind: .secureField, textFieldTitle: $viewModel.checkPassword)
-                            .focused($focusFeild, equals: .checkPassword)
+            ScrollViewReader(content: { proxy in
+                ZStack {
+                    ScrollView {
+                        VStack (spacing: 24) {
+                            TitleTextField(title: "이메일", isValid: $viewModel.state.isValidEmail, placeHolder: "이메일을 입력하세요", kind: .textField, textFieldTitle: $viewModel.email, splitView: {
+                                RoundedButton(action: {
+                                    
+                                }, label: {
+                                    Text("중복확인")
+                                }, backgroundColor: !viewModel.email.isEmpty ? .brandGreen : .brandInactive)
+                                .frame(width: 100)
+                                .id(RegisterViewModel.FocusTextField.email)
+                                .disabled(viewModel.email.isEmpty)
+                            })
+                            .focused($focusFeild, equals: .email)
+                            
+                            TitleTextField(title: "닉네임", isValid: $viewModel.state.isValidNick, placeHolder: "닉네임을 입력하세요", kind: .textField, textFieldTitle: $viewModel.nick)
+                                .focused($focusFeild, equals: .nick)
+                                .id(RegisterViewModel.FocusTextField.nick)
+                            
+                            TitleTextField(title: "연락처", isValid: $viewModel.state.isValidPhoneNumber, placeHolder: "전화번호를 입력하세요", kind: .textField, textFieldTitle: $viewModel.phoneNumber)
+                                .focused($focusFeild, equals: .phoneNumber).id(RegisterViewModel.FocusTextField.phoneNumber)
+                            
+                            TitleTextField(title: "비밀번호", isValid: $viewModel.state.isValidPassword, placeHolder: "비밀번호을 입력하세요", kind: .secureField, textFieldTitle: $viewModel.password)
+                                .focused($focusFeild, equals: .password).id(RegisterViewModel.FocusTextField.password)
+                            
+                            TitleTextField(title: "비밀번호 확인", isValid: $viewModel.state.passwordIsSame, placeHolder: "비밀번호를 한번 더 입력하세요", kind: .secureField, textFieldTitle: $viewModel.checkPassword)
+                                .focused($focusFeild, equals: .checkPassword).id(RegisterViewModel.FocusTextField.checkPassword)
+                            Spacer()
+                        }
+                        .padding(24)
+                    }
+                    .padding(.bottom, 55)
+                    VStack {
                         Spacer()
-                    }
-                    .padding(24)
-                }
-                        
-                VStack {
-                    Spacer()
-                        .toastView(toast: $viewModel.state.toastMessage)
-                    KeyboardStickeyButton(isFocus: .constant(focusFeild != nil), title: "가입하기", isEnable: $viewModel.state.canTapRegisterButton) {
-                        viewModel.trigger(.tapRegisterButton)
-                        focusFeild = viewModel.state.focusField
+                            .toastView(toast: $viewModel.state.toastMessage)
+                        KeyboardStickeyButton(isFocus: .constant(focusFeild != nil), title: "가입하기", isEnable: $viewModel.state.canTapRegisterButton) {
+                            viewModel.trigger(.tapRegisterButton)
+                            focusFeild = viewModel.state.focusField
+                            proxy.scrollTo(viewModel.state.focusField, anchor: .top)
+                        }
                     }
                 }
-            }
-            .defaultBackground()
-            .underlineNavigationBar(title: "회원가입")
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button(action: {
-                        isPresenting.toggle()
-                    }, label: {
-                        Image(systemName: "xmark")
-                            .foregroundStyle(.black)
-                    })
+                .defaultBackground()
+                .underlineNavigationBar(title: "회원가입")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button(action: {
+                            isPresenting.toggle()
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .foregroundStyle(.black)
+                        })
+                    }
                 }
-            }
+            })
+            
+            
         }//NavigationStack
         .presentationDragIndicator(.visible)
     }
