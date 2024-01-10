c29dd9467d71a51ff4741149e035c0f40689708f
Kim junhwan <tazanboy1@naver.com>
2024-01-10 23:52:37 +0900

    [feat] 네트워킹 규칙 정의

---
+++ ServiceLevelProject/Network/NetworkPolicy.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkPolicy.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/10.
+//
+
+import Foundation
+
+enum NetworkPolicy {
+    static let defaultTimeoutInterval = 10.0
+}
