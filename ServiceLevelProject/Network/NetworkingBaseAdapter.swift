c3f3df4f32816960930c45cd081a55f15825f73d
Kim junhwan <tazanboy1@naver.com>
2024-01-13 01:23:26 +0900

    [feat] 기본 네트워킹 adapter 구현

---
+++ ServiceLevelProject/Network/NetworkingBaseAdapter.swift
@@ -0,0 +1,29 @@
+//
+//  DefaultAdapter.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/10.
+//
+
+import Foundation
+import Alamofire
+
+struct NetworkingBaseAdapter: RequestAdapter {
+    
+    func adapt(_ urlRequest: URLRequest, for session: Alamofire.Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var urlRequest = urlRequest
+        let baseURL = "http://slp.sesac.kr:27888"
+        guard let SLPAPIKey = Bundle.main.infoDictionary?["SESAC_APP_KEY"] as? String,
+              let path = urlRequest.url?.absoluteString,
+              let requestURL = URL(string: "\(baseURL)\(path)")
+        else {
+            completion(.failure(DefaultNetworkingError.failSetBaseURLRequest))
+            return
+        }
+        urlRequest.headers.add(name: "SesacKey", value: SLPAPIKey)
+        urlRequest.url = requestURL
+        completion(.success(urlRequest))
+    }
+    
+}
+
