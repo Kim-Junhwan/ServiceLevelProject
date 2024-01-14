04a78339e96d4123eecba98bc8b9683d317d9412
Kim junhwan <tazanboy1@naver.com>
2024-01-15 02:49:07 +0900

    [feat] 회원가입 응답 Entity 정의

---
+++ ServiceLevelProject/Domain/Entities/Auth/RegistedUserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  RegistedUserProfile.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/14.
+//
+
+import Foundation
+
+struct RegistedUserProfile {
+    let userId: Int
+    let email: String
+    let nickName: String
+    let profileImage: String?
+    let phone: String?
+    let vendor: String?
+    let createdAt: String
+    let accessToken: String
+    let refreshToken: String
+}
