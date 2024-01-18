4c36474262a6ef9958f37a61f8e7cff9325408d4
Kim junhwan <tazanboy1@naver.com>
2024-01-18 21:09:48 +0900

    [feat] 인증Scene DIContainer 구현

--- ServiceLevelProject/App/DIContainer/AuthorizationSceneDIContainer.swift
+++ ServiceLevelProject/App/DIContainer/AuthorizationSceneDIContainer.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+
+final class AuthorizationSceneDIContainer {
+    
+    lazy var userLoginStatusManager: UserLoginStatusManager = {
+       return UserLoginStatusManager()
+    }()
+    
+    func makeRegisterUseCase() -> RegisterUserUseCase {
+        return DefaultRegisterUserUseCase(authRepository: makeAuthRepository(), loginInfoRepository: userLoginStatusManager)
+    }
+    
+    func makeCheckEmailUseCase() -> CheckEmailUseCase {
+        return DefaultCheckEmailUseCase(authRepository: makeAuthRepository())
+    }
+    
+    func makeAuthRepository() -> AuthRepository {
+        return DefaultAuthRepository()
+    }
+    
+    func makeRegisterView(presenting: Binding<Bool>) -> some View {
+        return RegisterView(isPresenting: presenting, viewModel: .init(authDIContainer: self, checkEmailUseCase: self.makeCheckEmailUseCase(), registerUseCase: self.makeRegisterUseCase()))
+    }
+    
+    func makeLoginView() -> some View {
+        return LoginView(viewModel: .init(authDIContainer: self))
+    }
+}
