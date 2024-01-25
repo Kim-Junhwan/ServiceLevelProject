4e1a74b53518b9fb8463ce826fe3325fbf1ffcc8
Kim junhwan <tazanboy1@naver.com>
2024-01-25 18:14:12 +0900

    [feat] LoginUseCase LoginType 변경

--- ServiceLevelProject/Domain/Auth/UseCases/LoginUseCase.swift
+++ ServiceLevelProject/Domain/Auth/UseCases/LoginUseCase.swift
@@ -7,12 +7,6 @@
 
 import Foundation
 
-enum LoginPlatform {
-    case kakao
-    case apple(idToken: String, nickName: String?)
-    case email(email: String, password: String)
-}
-
 protocol LoginUseCase {
     func excute(_ platform: LoginType) async throws
 }
@@ -31,18 +25,22 @@ final class DefaultLoginUseCase {
 extension DefaultLoginUseCase: LoginUseCase {
     func excute(_ platform: LoginType) async throws {
         let userProfile: UserProfile
+        let loginPlatform: LoginPlatform
         switch platform {
         case .apple(let idToken, let nickName):
             userProfile = try await authRepository.appleLogin(.init(idToken: idToken, nickName: nickName, deviceToken: deviceToken))
+            loginPlatform = .apple(idToken: idToken)
         case .kakao:
             let info = try await KakaoLoginManager().login()
             userProfile = try await authRepository.kakaoLogin(.init(oauthToken: info, deviceToken: deviceToken))
+            loginPlatform = .kakao(oauthToken: info)
         case .email(let email, let password):
             userProfile = try await authRepository.emailLogin(.init(email: email, password: password, deviceToken: deviceToken))
+            loginPlatform = .email(email: email, password: password)
         case .none:
             fatalError()
         }
-        appState.loginInfo.loginType = platform
+        appState.loginInfo.loginType = loginPlatform
         appState.userData.nickname = userProfile.nickName
         appState.userData.profileImagePath = userProfile.profileImage
         appState.token.accessToken = userProfile.accessToken
