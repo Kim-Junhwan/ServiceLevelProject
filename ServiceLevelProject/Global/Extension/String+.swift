56c41ad945eb1aace838e5639eeeab8236937240
Kim junhwan <tazanboy1@naver.com>
2024-01-28 23:31:04 +0900

    [feat] String -> Date 변환 기능 추가

--- ServiceLevelProject/Global/Extension/String+.swift
+++ ServiceLevelProject/Global/Extension/String+.swift
@@ -7,19 +7,29 @@
 
 import Foundation
 
+enum FormattingError: Error {
+    case stringToDateFormatError
+}
+
 extension String {
-  // MARK: - 휴대폰 번호 하이픈 추가
-  public var withHypen: String {
-    var stringWithHypen: String = self
-      if self.count >= 10 {
-          stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
-          stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.endIndex, offsetBy: -4))
-      } else if self.count >= 7 {
-          stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
-          stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 7))
-      } else if self.count >= 4 {
-          stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
-      }
-    return stringWithHypen
-  }
+    
+    func phoneFormat() -> Self {
+        var stringWithHypen: String = self
+          if self.count >= 10 {
+              stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
+              stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.endIndex, offsetBy: -4))
+          } else if self.count >= 7 {
+              stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
+              stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 7))
+          } else if self.count >= 4 {
+              stringWithHypen.insert("-", at: stringWithHypen.index(stringWithHypen.startIndex, offsetBy: 3))
+          }
+        return stringWithHypen
+    }
+    
+    func toDate() throws -> Date {
+        let dateFormatter = ISO8601DateFormatter()
+        guard let date = dateFormatter.date(from: self) else { throw FormattingError.stringToDateFormatError }
+        return date
+    }
 }
