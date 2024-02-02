c2b51df08271c13d6505519a4b7f63fdf980508f
Kim junhwan <tazanboy1@naver.com>
2024-02-02 23:48:40 +0900

    [feat] 워크스페이스 목록 뷰 정의

---
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceListView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkspaceListView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/02.
+//
+
+import SwiftUI
+
+struct WorkspaceListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkspaceListView()
+}
