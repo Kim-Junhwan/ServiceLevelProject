8087cb75f060f1ab4ea349c78e71f465722b0853
Kim junhwan <tazanboy1@naver.com>
2024-02-03 23:10:31 +0900

    [feat] 사이드 메뉴 View 구현

---
+++ ServiceLevelProject/Presentation/CommonView/SideMenu.swift
@@ -0,0 +1,34 @@
+//
+//  SideMenu.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/03.
+//
+
+import SwiftUI
+
+struct SideMenu: View {
+    
+    @Binding var isPresenting: Bool
+    var content: AnyView
+    var edgeTransition: AnyTransition = .move(edge: .leading)
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            if isPresenting {
+                Color.black
+                    .opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        isPresenting = false
+                    }
+                content
+                    .transition(edgeTransition)
+                    .background(.clear)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+        .animation(.easeInOut, value: isPresenting)
+    }
+}
