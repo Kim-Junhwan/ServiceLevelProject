87f814d42c6bda61f7e741cd55e9b3668348c121
Kim junhwan <tazanboy1@naver.com>
2024-02-04 03:07:27 +0900

    [feat] 빈 워크스페이스  메뉴 뷰 구현

--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/SideMenuEmptyWorkspace.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/SideMenuEmptyWorkspace.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct SideMenuEmptyWorkspace: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("워크스페이스를 찾을 수 없어요.")
+                .font(CustomFont.title1.font)
+            Text("관리자에게 초대를 요청하거나, 다른 이메일로 시도하거나 새로운 워크스페이스를 생성해주세요. ")
+                .font(CustomFont.body.font)
+            
+            RoundedButton(action: {
+                
+            }, label: {
+                Text("워크스페이스 생성")
+            }, backgroundColor: .brandGreen)
+        }
+        .multilineTextAlignment(.center)
     }
 }
 
