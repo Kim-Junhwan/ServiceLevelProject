4f6523796b22fe762d72f72858ef11ba918c180b
Kim junhwan <tazanboy1@naver.com>
2024-02-04 03:07:00 +0900

    [feat] 워크스페이스 메뉴 빈 경우, 비지 않는 경우의 뷰 생성

---
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/SideMenuEmptyWorkspace.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuEmprtWorkspace.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/04.
+//
+
+import SwiftUI
+
+struct SideMenuEmptyWorkspace: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuEmptyWorkspace()
+}
