d1db6bcad7b03704db8d8c02bf4b0f066f87e7a9
Kim junhwan <tazanboy1@naver.com>
2024-02-03 23:44:38 +0900

    [refactor] 홈뷰에 사이드메뉴 추가

--- ServiceLevelProject/Presentation/Channel/HomeView.swift
+++ ServiceLevelProject/Presentation/Channel/HomeView.swift
@@ -10,44 +10,55 @@ import SwiftUI
 struct HomeView: View {
     
     @State var hasChatList: Bool = true
+    @State var showWorkspaceList: Bool = false
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                if hasChatList {
-                    HomeTabView()
-                } else {
-                    EmptyHomeView()
-                }
-            }
-            .onAppear {
-                Task {
-                    do {
-                        let value = try await DefaultWorkspaceRepository().fetchComeInWorkspaceList()
+        ZStack {
+            NavigationStack {
+                VStack {
+                    if hasChatList {
+                        HomeTabView()
+                    } else {
+                        EmptyHomeView()
                     }
                 }
-            }
-            .underlineNavigationBar(title: "")
-            .toolbarBackground(.white, for: .navigationBar)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    HStack {
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Image(systemName: "star")
+                .onAppear {
+                    Task {
+                        do {
+                            let value = try await DefaultWorkspaceRepository().fetchComeInWorkspaceList()
+                        }
+                    }
+                }
+                .underlineNavigationBar(title: "")
+                .toolbarBackground(.white, for: .navigationBar)
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        HStack {
+                            Button(action: {
+                                showWorkspaceList = true
+                            }, label: {
+                                Image(systemName: "star")
+                                    .resizable()
+                                    .frame(width: 32, height: 32)
+                                    .background(.red)
+                                    .clipShape(.rect(cornerRadius: 8))
+                            })
+                            Text("No WorkSpace")
+                                .font(CustomFont.title1.font)
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "xmark")
                         })
-                        Text("No WorkSpace")
-                            .font(CustomFont.title1.font)
                     }
                 }
                 
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        
-                    }, label: {
-                        Image(systemName: "xmark")
-                    })
-                }
             }
+            SideMenu(isPresenting: $showWorkspaceList, content: AnyView(WorkspaceListView(isPresenting: $showWorkspaceList)))
         }
     }
 }
