1d2720c919acb1cdd2536107c7b3e01205fc1081
Kim junhwan <tazanboy1@naver.com>
2024-02-03 23:43:45 +0900

    [feat] 워크스페이스 사이드 메뉴 navigationbar 추가

--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceListView.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceListView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct WorkspaceListView: View {
+    
+    @Binding var isPresenting: Bool
+    
+    func setNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .backgroundPrimary
+        appearance.shadowColor = .clear
+        appearance.largeTitleTextAttributes = [.font: CustomFont.title1.uifont]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+    }
+    
+    init(isPresenting: Binding<Bool>) {
+        self._isPresenting = isPresenting
+        setNavigationBar()
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ZStack {
+                NavigationStack {
+                    List {
+                        
+                    }
+                    .navigationTitle("워크스페이스")
+                }
+                .frame(width: 270)
+            }
+            Spacer()
+        }
+        .background(.clear)
+        
     }
 }
 
 #Preview {
-    WorkspaceListView()
+    WorkspaceListView(isPresenting: .constant(true))
 }
