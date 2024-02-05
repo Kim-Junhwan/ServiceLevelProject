5ce02ed0f705f45ab473a5a8c24c219a398f2178
Kim junhwan <tazanboy1@naver.com>
2024-02-06 00:50:34 +0900

    [refactor] 사이드 메뉴 뷰 터치 안되던 버그 수정 및 레이아웃 디테일 작업

--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceListView.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceListView.swift
@@ -10,40 +10,88 @@ import SwiftUI
 struct WorkspaceListView: View {
     
     @Binding var isPresenting: Bool
-    
-    func setNavigationBar() {
-        let appearance = UINavigationBarAppearance()
-        appearance.backgroundColor = .backgroundPrimary
-        appearance.shadowColor = .clear
-        appearance.largeTitleTextAttributes = [.font: CustomFont.title1.uifont]
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-    }
-    
-    init(isPresenting: Binding<Bool>) {
-        self._isPresenting = isPresenting
-        setNavigationBar()
-    }
+    @State var workspaceList: [Int] = [1,2,3]
     
     var body: some View {
-        HStack {
-            ZStack {
-                NavigationStack {
-                    List {
-                        
+            HStack {
+                GeometryReader { proxy in
+                    ZStack(alignment: .bottom) {
+                        Rectangle()
+                            .fill(.white)
+                            .frame(width: proxy.size.width * 0.8)
+                            .clipShape(.rect(cornerRadius: 20))
+                        VStack(alignment: .leading, spacing: 0) {
+                            Rectangle()
+                                .fill(.backgroundPrimary)
+                                .frame(height:100)
+                                .clipShape(.rect(topTrailingRadius: 20))
+                                .overlay(alignment: .bottomLeading) {
+                                    Text("워크스페이스")
+                                        .font(CustomFont.title1.font)
+                                        .padding(17)
+                                }
+                            workspaceContent
+                            .navigationTitle("워크스페이스")
+                            .toolbar(.visible, for: .navigationBar)
+                            VStack(alignment: .leading) {
+                                SideMenuOptionButton(title: "워크스페이스 추가", image: Image(systemName: "plus")) {
+                                    print("워크스페이스 추가")
+                                }
+                                SideMenuOptionButton(title: "도움말", image: Image(systemName: "questionmark.circle")) {
+                                    print("도움말")
+                                }
+                            }
+                            .offset(y: -proxy.safeAreaInsets.bottom)
+                            .padding([.leading], 16)
+                        }
+                        .background(.clear)
+                        .frame(width: proxy.size.width * 0.8)
+                        .onAppear {
+                            print(proxy.size)
+                            print(proxy.safeAreaInsets)
+                        }
                     }
-                    .navigationTitle("워크스페이스")
+                    .ignoresSafeArea()
                 }
-                .frame(width: 270)
+                Spacer()
             }
-            Spacer()
-        }
-        .background(.clear)
         
+    }
+    
+    @ViewBuilder
+    var workspaceContent: some View {
+        if workspaceList.isEmpty {
+            SideMenuEmptyWorkspace()
+                .padding(24)
+        } else {
+            SideMenuWorkspaceListView()
+                .padding([.leading, .trailing], 6)
+        }
+    }
+}
+
+struct SideMenuOptionButton: View {
+    let title: String
+    let image: Image
+    let action: ()-> Void
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }, label: {
+            HStack(spacing: 16) {
+                image
+                    .foregroundStyle(.textSecondary)
+                Text(title)
+                    .foregroundStyle(.textSecondary)
+                    .font(CustomFont.caption.font)
+            }
+        })
+        .frame(height: 41)
     }
 }
 
 #Preview {
     WorkspaceListView(isPresenting: .constant(true))
+        .environmentObject(AppState())
 }
