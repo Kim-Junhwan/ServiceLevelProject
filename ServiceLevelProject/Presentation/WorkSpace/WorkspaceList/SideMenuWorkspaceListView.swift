94926cbca8578ed50f2dbc9cb13b4984b18590a7
Kim junhwan <tazanboy1@naver.com>
2024-02-07 00:00:18 +0900

    [feat] 사이드 메뉴 리스트 화면 구현

--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/SideMenuWorkspaceListView.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/SideMenuWorkspaceListView.swift
@@ -7,12 +7,79 @@
 
 import SwiftUI
 
+struct WorkspaceThumbnailModel: Identifiable {
+    let id = UUID()
+    let title: String
+    let createdAt: String
+    let imagePath: String
+}
+
 struct SideMenuWorkspaceListView: View {
+    @Binding var workspaceList: [WorkspaceThumbnailModel]
+    @State private var selection: UUID?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        List(workspaceList) { workspace in
+            WorkspaceSideMenuCell(workspace: workspace, isSelected: workspace.id == selection) {
+                selection = workspace.id
+            }
+            .listRowInsets(.init(top: 6, leading: 2, bottom: 6, trailing: 2))
+            .listRowSeparator(.hidden)
+        }
+        .listStyle(PlainListStyle())
+        .scrollIndicators(.hidden)
+    }
+}
+
+struct WorkspaceSideMenuCell: View {
+    
+    let workspace: WorkspaceThumbnailModel
+    var isSelected: Bool
+    let action: ()-> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            HStack {
+                FetchImageFromServerView(url: workspace.imagePath, placeHolder: {
+                    Image(.workspaceBallon)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 44, height: 44)
+                        .offset(y: 5)
+                        .background(.brandGreen)
+                })
+                .frame(width: 44, height: 44)
+                .clipShape(.rect(cornerRadius: 8))
+                .padding(.trailing, 8)
+                
+                VStack(alignment: .leading) {
+                    Text(workspace.title)
+                        .font(CustomFont.bodyBold.font)
+                    Text(workspace.createdAt)
+                        .font(CustomFont.body.font)
+                }
+                Spacer()
+                
+                Button(action: {
+                    print("Hello")
+                }, label: {
+                    Image(systemName: "ellipsis")
+                        .scaledToFit()
+                        .foregroundStyle(.black)
+                        .frame(width: 20, height: 20)
+                })
+            }
+            .padding(8)
+            .frame(maxWidth: .infinity)
+            .background(isSelected ? .brandGray : .white, in: RoundedRectangle(cornerRadius: 10))
+        }
     }
 }
 
 #Preview {
-    SideMenuWorkspaceListView()
+    SideMenuWorkspaceListView(workspaceList: .constant([]))
+        .environmentObject(AppState())
 }
