f13b9c4c9df391ab2ec3f2233d3b6c3d8b7d2cf1
Kim junhwan <tazanboy1@naver.com>
2024-02-07 16:11:30 +0900

    [feat] ChatListView 추가

---
+++ ServiceLevelProject/Presentation/Channel/ChatListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/01/19.
+//
+
+import SwiftUI
+
+struct ChatListView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ChatListView()
+}
