c82615df41c489d705ebae4e173e6bbe943946d0
Kim junhwan <tazanboy1@naver.com>
2024-02-15 20:14:05 +0900

    [feat] 워크스페이스 ActionSheet State Environment 의존하도록 변경

--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceActionSheetView.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceList/WorkspaceActionSheetView.swift
@@ -9,37 +9,34 @@ import SwiftUI
 
 struct WorkspaceActionSheetView: View {
     let isAdmin: Bool
-    @State var workspaceOutAlert = false
-    @State var adminWorkspaceOut = false
-    @Binding var editWorkspace: Bool
-    @State var workspaceDelete = false
+    @EnvironmentObject var state: WorkspaceListViewState
     
     var body: some View {
         if isAdmin {
             Button("워크스페이스 편집") {
-                editWorkspace = true
+                state.showEditWorkspace = true
             }
             
             Button("워크스페이스 나가기") {
-                adminWorkspaceOut = true
-            }
-            .customAlert(title: "워크스페이스 나가기", description: "회원님은 워크스페이스 관리자입니다. 워크스페이스 관리자를 다른 멤버로 변경 한 후 나갈 수 있습니다.", actionTitle: "확인", isPresenting: $adminWorkspaceOut) {
-                adminWorkspaceOut = false
+                state.showAlert = .init(title: "워크스페이스 나가기", description: "회원님은 워크스페이스 관리자입니다. 워크스페이스 관리자를 다른 멤버로 변경 한 후 나갈 수 있습니다.", type: .ok(title: "확인"), action: {
+                    state.showAlert = nil
+                })
             }
             
             Button("워크스페이스 관리자 변경") {
-                
+                state.changeWorkspaceAdmin = true
             }
             
             Button("워크스페이스 삭제", role: .destructive) {
-                workspaceDelete = true
-            }
-            .customAlert(title: "워크스페이스 삭제", description: "정말 이 워크스페이스를 삭제하시겠습니까? 삭제 시 채널/멤버/채팅 등 워크스페이스 내의 모든 정보가 삭제되며 복구할 수 없습니다.", cancelTitle: "취소", actionTitle: "확인", isPresenting: $workspaceDelete) {
-                workspaceDelete = false
+                state.showAlert = .init(title: "워크스페이스 삭제", description: "정말 이 워크스페이스를 삭제하시겠습니까? 삭제 시 채널/멤버/채팅 등 워크스페이스 내의 모든 정보가 삭제되며 복구할 수 없습니다.", type: .cancelOk(cancelTitle: "취소", okTitle: "삭제"), action: {
+                    
+                })
             }
         } else {
             Button("워크스페이스 나가기") {
-                workspaceOutAlert = true
+                state.showAlert = .init(title: "워크스페이스 나가기", description: "정말 이 워크스페이스를 떠나시겠습니까?", type: .cancelOk(cancelTitle: "취소", okTitle: "나가기"), action: {
+                    
+                })
             }
         }
         
