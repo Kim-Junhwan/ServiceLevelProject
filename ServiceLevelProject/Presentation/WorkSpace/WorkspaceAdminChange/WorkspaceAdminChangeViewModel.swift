fdd0104d8c4018e7a9590226ca62f07e1c95cc3f
Kim junhwan <tazanboy1@naver.com>
2024-02-15 15:31:15 +0900

    [feat] Workspace 관리자 변경 View, ViewModel 구현

--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceAdminChange/WorkspaceAdminChangeViewModel.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceAdminChange/WorkspaceAdminChangeViewModel.swift
@@ -4,5 +4,61 @@
 //
 //  Created by JunHwan Kim on 2024/02/15.
 //
-
 import Foundation
+import Combine
+
+struct WorkspaceMemberModel: Identifiable {
+    let id: UUID = .init()
+    let userId: Int
+    let email: String
+    let nickname: String
+    let profileImagePath: String?
+}
+
+class WorkspaceAdminChangeViewModel: ViewModel {
+    enum WorkSpaceAdminChangeInput {
+        case appearView
+        case tapMember
+    }
+    
+    struct WorkSpaceAdminChangeState {
+        var canChangeWorkspaceAdmin: Bool = true
+    }
+    
+    @Published var state: WorkSpaceAdminChangeState = .init()
+    @Published var members: [WorkspaceMemberModel] = []
+    private var cancellableBag = Set<AnyCancellable>()
+    private let workspaceId: Int
+    private let changeWorkspaceAdminUseCase: ChangeWorkspaceAdminUseCase
+    
+    init(workspaceId: Int, changeWorkspaceAdminUseCase: ChangeWorkspaceAdminUseCase) {
+        self.workspaceId = workspaceId
+        self.changeWorkspaceAdminUseCase = changeWorkspaceAdminUseCase
+    }
+    
+    func trigger(_ input: WorkSpaceAdminChangeInput) {
+        switch input {
+        case .appearView:
+            fetchWorkspaceMember()
+        case .tapMember:
+            break
+        }
+    }
+    
+    private func fetchWorkspaceMember() {
+        Task { @MainActor in
+            do {
+                let value = try await changeWorkspaceAdminUseCase.fetchWorkspaceMember(.init(workspaceId: workspaceId))
+                DispatchQueue.main.async {
+                    if value.isEmpty {
+                        self.state.canChangeWorkspaceAdmin = false
+                    } else {
+                        self.members = value.map{ .init(userId: $0.id, email: $0.email, nickname: $0.nickname, profileImagePath: $0.profileImagePath) }
+                    }
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
--- ServiceLevelProject/Presentation/WorkSpace/WorkspaceAdminChange/WorkspaceAdminView.swift
+++ ServiceLevelProject/Presentation/WorkSpace/WorkspaceAdminChange/WorkspaceAdminView.swift
@@ -7,12 +7,89 @@
 
 import SwiftUI
 
-struct WorkspaceAdminView: View {
+struct WorkspaceChangeAdminView: View {
+    @Binding var isPresenting: Bool
+    @StateObject var viewModel: WorkspaceAdminChangeViewModel
+    @State var showAlert: Bool = false
+    
+    init(isPresenting: Binding<Bool>, workspaceId: Int) {
+        self._isPresenting = isPresenting
+        self._viewModel = StateObject(wrappedValue: SharedAssembler.shared.resolve(WorkspaceAdminChangeViewModel.self, argument: workspaceId))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.members) { member in
+                        WorkspaceChangeAdminMemberCell(member: member)
+                            .padding([.leading, .trailing], 14)
+                            .padding([.top, .bottom], 8)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .defaultBackground()
+            .underlineNavigationBar(title: "워크스페이스 관리자 변경")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        isPresenting = false
+                    }, label: {
+                        Image(systemName: "xmark")
+                            .foregroundStyle(.black)
+                    })
+                }
+            }
+            .onAppear {
+                viewModel.trigger(.appearView)
+            }
+            .onChange(of: viewModel.state.canChangeWorkspaceAdmin) { value in
+                showAlert = !value
+            }
+            .fullScreenCover(isPresented: $showAlert, content: {
+                CustomAlertView(title: "워크스페이스 관리자 변경 불가", description: "워크스페이스 멤버가 없어 관리자 변경을 할 수 없습니다. 새로운 멤버를 워크스페이스에 포대해보세요.", actionTitle: "확인", isPresenting: $showAlert) {
+                    showAlert = false
+                    isPresenting = false
+                }
+            })
+        }
+        
+    }
+}
+
+struct WorkspaceChangeAdminMemberCell: View {
+    let member: WorkspaceMemberModel
+    
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            HStack {
+                FetchImageFromServerView(url: member.profileImagePath) {
+                    Image(.noPhotoGreen)
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.white)
+                }
+                .frame(width: 44, height: 44)
+                .clipShape(.rect(cornerRadius: 8))
+                .padding(.trailing, 8)
+                
+                VStack(alignment: .leading) {
+                    Text(member.nickname)
+                        .font(CustomFont.bodyBold.font)
+                        .foregroundStyle(.textPrimary)
+                    Text(member.email)
+                        .font(CustomFont.body.font)
+                        .foregroundStyle(.textSecondary)
+                }
+                Spacer()
+            }
+        })
     }
 }
 
 #Preview {
-    WorkspaceAdminView()
+    WorkspaceChangeAdminView(isPresenting: .constant(true), workspaceId: 1)
 }
