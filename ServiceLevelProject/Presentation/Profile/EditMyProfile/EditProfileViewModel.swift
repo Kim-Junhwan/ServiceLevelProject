4007a53d0f6a9736e2bbc2f056183d9b9a30fba3
Kim junhwan <tazanboy1@naver.com>
2024-02-19 23:53:21 +0900

    [feat] EditProfileViewModel에 닉네임, 전화번호, 프로필 이미지 수정 기능 추가

--- ServiceLevelProject/Presentation/Profile/EditMyProfile/EditProfileViewModel.swift
+++ ServiceLevelProject/Presentation/Profile/EditMyProfile/EditProfileViewModel.swift
@@ -12,6 +12,8 @@ final class EditProfileViewModel: ViewModel {
     
     enum EditProfileInput {
         case logout
+        case editNickname(String)
+        case editPhoneNumber(String)
         case onAppear
     }
     
@@ -22,6 +24,8 @@ final class EditProfileViewModel: ViewModel {
         var email: String
         var toast: Toast?
         let vendor: LoginPlatform
+        var nicknameEditingSuccess: Bool = false
+        var phoneEditingSuccess: Bool = false
     }
     
     @Published var imageModel: ImagePickerModel = ImagePickerModel(maxSize: 70, imageData: nil)
@@ -35,7 +39,17 @@ final class EditProfileViewModel: ViewModel {
         self.state = EditProfileViewState(sesacCoin: userData.sesacCoin, nickname: userData.nickname, phoneNumber: userData.phone, email: userData.email, vendor: appState.loginInfo.loginType)
         self.appState = appState
         self.profileRepository = profileRepository
-        appStateBind()
+        imageModel.action = { result in
+            switch result {
+            case .success(let data):
+                if let imageData = data {
+                    self.editProfileImage(imageData: imageData)
+                }
+            case .failure(_):
+                print("Fail")
+            }
+        }
+        bind()
     }
     
     func trigger(_ input: EditProfileInput) {
@@ -44,15 +58,24 @@ final class EditProfileViewModel: ViewModel {
             appState.logout()
         case .onAppear:
             fetchMyProfile()
+        case .editNickname(let nickname):
+            editNickname(nickName: nickname)
+        case .editPhoneNumber(let phoneNumber):
+            editPhone(phoneNumber: phoneNumber)
         }
     }
     
-    func appStateBind() {
+    func bind() {
+        appStateBind()
+    }
+    
+    private func appStateBind() {
         appState.$userData
             .receive(on: RunLoop.main)
             .sink { userData in
                 self.state.nickname = userData.nickname
                 self.state.email = userData.email
+                self.imageModel.url = userData.profileImagePath
                 self.state.phoneNumber = userData.phone
                 self.state.sesacCoin = userData.sesacCoin
             }
@@ -66,6 +89,36 @@ final class EditProfileViewModel: ViewModel {
                 appState.setUserData(userProfile)
             } catch {
                 self.state.toast = .init(message: error.localizedDescription, duration: 1.0)
+            }
+        }
+    }
+    
+    private func editNickname(nickName: String) {
+        Task {
+            let userProfile = try await profileRepository.editProfile(.init(nickname: nickName, phone: state.phoneNumber))
+            appState.setUserData(userProfile)
+            DispatchQueue.main.async {
+                self.state.nicknameEditingSuccess = true
+            }
+        }
+    }
+    
+    private func editPhone(phoneNumber: String) {
+        Task {
+            let userProfile = try await profileRepository.editProfile(.init(nickname: state.nickname, phone: phoneNumber))
+            appState.setUserData(userProfile)
+            DispatchQueue.main.async {
+                self.state.phoneEditingSuccess = true
+            }
+        }
+    }
+    
+    private func editProfileImage(imageData: Data) {
+        Task {
+            let userProfile = try await profileRepository.editProfileImage(imageData: imageData)
+            appState.setUserData(userProfile)
+            DispatchQueue.main.async {
+                self.state.toast = .init(message: "프로필 이미지가 성공적으로 변경되었습니다.", duration: 1.0)
             }
         }
     }
