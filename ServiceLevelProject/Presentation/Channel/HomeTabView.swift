4a34eed43cb042779b0ce33df2935fb91f8453df
Kim junhwan <tazanboy1@naver.com>
2024-02-26 02:03:43 +0900

    [fix] 탭바 선택 색상 변경 안되던 버그 수정

--- ServiceLevelProject/Presentation/Channel/HomeTabView.swift
+++ ServiceLevelProject/Presentation/Channel/HomeTabView.swift
@@ -10,57 +10,55 @@ import SwiftUI
 struct HomeTabView: View {
     var body: some View {
         TabView {
-            ChatListView()
-                .tabItem {
-                    Label(
-                        title: { Text("홈") },
-                        icon: {
-                            Image(.list)
-                                .renderingMode(.template).resizable()
-                        }
-                    )
+            Group {
+                ChatListView()
+                    .tabItem {
+                        Label(
+                            title: { Text("홈") },
+                            icon: {
+                                Image(.list)
+                                    .renderingMode(.template).resizable()
+                            }
+                        )
                 }
-            
-            DMView()
-                .tabItem {
-                    Label(
-                        title: { Text("DM") },
-                        icon: {
-                            Image(.rightArrow)
-                                .renderingMode(.template).resizable()
-                        }
-                    )
-                }
-            
-            SearchView()
-                .tabItem {
-                    Label(
-                        title: { Text("검색") },
-                        icon: {
-                            Image(.smile)
-                                .renderingMode(.template).resizable()
-                        }
-                    )
-                }
-            
-            OptionView()
-                .tabItem {
-                    Label(
-                        title: { Text("설정") },
-                        icon: {
-                            Image(.window)
-                                .renderingMode(.template).resizable()
-                        }
-                    )
-                }
+                
+                DMView()
+                    .tabItem {
+                        Label(
+                            title: { Text("DM") },
+                            icon: {
+                                Image(.rightArrow)
+                                    .renderingMode(.template).resizable()
+                            }
+                        )
+                    }
+                
+                SearchView()
+                    .tabItem {
+                        Label(
+                            title: { Text("검색") },
+                            icon: {
+                                Image(.smile)
+                                    .renderingMode(.template).resizable()
+                            }
+                        )
+                    }
+                
+                OptionView()
+                    .tabItem {
+                        Label(
+                            title: { Text("설정") },
+                            icon: {
+                                Image(.window)
+                                    .renderingMode(.template).resizable()
+                            }
+                        )
+                    }
+            }
+            .toolbarBackground(.visible, for: .tabBar)
+            .toolbarBackground(.red, for: .tabBar)
         }
-        .tint(.black)
-        .onAppear {
-            let appearance = UITabBarAppearance()
-            appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
-            appearance.backgroundColor = UIColor(Color.orange.opacity(0.2))
-            UITabBar.appearance().standardAppearance = appearance
-        }
+        .accentColor(.black)
     }
 }
 
