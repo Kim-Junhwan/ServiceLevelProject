0f7bed7a860d3d35eea4d627d5c34b9b8534ebcb
Kim junhwan <tazanboy1@naver.com>
2024-02-26 03:06:07 +0900

    [feat] 빈 워크스페이스 멤버 뷰 구현

--- ServiceLevelProject/Presentation/Channel/DM/EmptyMemberDMView.swift
+++ ServiceLevelProject/Presentation/Channel/DM/EmptyMemberDMView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct EmptyMemberDMView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("워크스페이스에 멤버가 없어요.")
+                .font(CustomFont.title1.font)
+            Text("새로운 팀원을 초대해보세요.")
+                .font(CustomFont.body.font)
+            
+            RoundedButton(action: {
+                
+            }, label: {
+                Text("팀원 초대하기")
+            }, backgroundColor: .brandGreen)
+        }
+        .padding([.leading, .trailing], 62)
+        .multilineTextAlignment(.center)
     }
 }
 
