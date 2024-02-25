a0100cb262186a446fac4dbe38c7974f81d900ef
Kim junhwan <tazanboy1@naver.com>
2024-02-26 03:06:26 +0900

    [feat] 워크스페이스 멤버에서 자기 자신은 제외

--- ServiceLevelProject/Presentation/Channel/DM/DMViewModel.swift
+++ ServiceLevelProject/Presentation/Channel/DM/DMViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+final class DMViewModel: ViewModel, ObservableObject {
+    enum DMViewModelInput {
+        
+    }
+    
+    struct DMViewModelState {
+        var workspaceMembers: [UserThumbnailModel]
+        
+    }
+    
+    @Published var state: DMViewModelState
+    private let appState: AppState
+    
+    init(appState: AppState) {
+        self.appState = appState
+        guard let members = appState.selectWorkspace?.workspaceMembers.filter({ $0.id != appState.userData.id }) else { fatalError() }
+        self.state = .init(workspaceMembers: members.map{ .init(userThumnail: $0) })
+    }
+    
+    func trigger(_ input: DMViewModelInput) {
+        
+    }
+}
