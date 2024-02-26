ff529c40d7a799f9de42ea5e3d45552471ae0870
Kim junhwan <tazanboy1@naver.com>
2024-02-26 14:42:53 +0900

    [feat] 멤버 초대 화면 구현

---
+++ ServiceLevelProject/Presentation/Channel/InviteMember/InviteMemberView.swift
@@ -0,0 +1,48 @@
+//
+//  InviteMemberView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/26.
+//
+
+import SwiftUI
+
+struct InviteMemberView: View {
+    @Binding var isPresenting: Bool
+    @State var email: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                VStack {
+                    TitleTextField(title: "이메일", isValid: .constant(true), placeHolder: "초대하려는 팀원의 이메일을 입력하세요", kind: .textField, textFieldTitle: $email)
+                    Spacer()
+                }
+                .underlineNavigationBar(title: "팀원 초대")
+                .padding(24)
+                .defaultBackground()
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button(action: {
+                            isPresenting = false
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .foregroundStyle(.black)
+                        })
+                    }
+            }
+                VStack {
+                    Spacer()
+                    KeyboardStickeyButton(isFocus: .constant(false), title: "초대 보내기", isEnable: .constant(!email.isEmpty)) {
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    InviteMemberView(isPresenting: .constant(true))
+}
