98a13f4f0fe09def6466462393f59ef09f6163fc
Kim junhwan <tazanboy1@naver.com>
2024-02-28 04:50:20 +0900

    [feat] DateFormatter 날짜 포맷 기능 추가

--- ServiceLevelProject/Global/Extension/DateFormatter+.swift
+++ ServiceLevelProject/Global/Extension/DateFormatter+.swift
@@ -13,4 +13,27 @@ extension DateFormatter {
         df.dateFormat = "yy. MM. dd"
         return df
     }()
+    
+    static let hourMinuteDateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ko_KR")
+        df.dateFormat = "hh:mm a"
+        return df
+    }()
+    
+    static let dayHourMinuteDateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.locale = Locale(identifier: "ko_KR")
+        df.dateFormat = "MM/dd hh:mm a"
+        return df
+    }()
+    
+    static func todatStandardFormatting(_ date: Date) -> String {
+        let today = Date()
+        if Calendar.current.isDate(date, inSameDayAs: today) {
+            return DateFormatter.hourMinuteDateFormatter.string(from: date)
+        } else {
+            return DateFormatter.dayHourMinuteDateFormatter.string(from: date)
+        }
+    }
 }
