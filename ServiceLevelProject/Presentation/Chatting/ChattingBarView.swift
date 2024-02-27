91b3c97b1130b1d184fcc6bafa1b8d750290bb2e
Kim junhwan <tazanboy1@naver.com>
2024-02-27 23:28:59 +0900

    [feat] 채팅바 textfield, 이미지 추가 기능 구현

---
+++ ServiceLevelProject/Presentation/Chatting/ChattingBarView.swift
@@ -0,0 +1,121 @@
+//
+//  ChattingBarView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/27.
+//
+
+import SwiftUI
+import _PhotosUI_SwiftUI
+
+struct ChattingBarView: View {
+    @Binding var text: String
+    @State private var textViewHeight: CGFloat = 32
+    @State private var showPhotosPicker: Bool = false
+    @ObservedObject var imagePickerModel: MultipleImagePickerModel
+    
+    var body: some View {
+        HStack(alignment: .bottom) {
+            galleryButton
+            VStack {
+                chattingTextField
+                if !imagePickerModel.imageData.isEmpty {
+                    ScrollView(.horizontal) {
+                        LazyHStack(spacing: 6) {
+                            ForEach(imagePickerModel.imageState) { state in
+                                pickerImageCell(state: state)
+                                    .frame(width: 50, height: 50)
+                            }
+                        }
+                    }
+                    .frame(height: 60)
+                }
+            }
+            sendButton
+        }
+        .padding([.leading, .trailing], 12)
+        .padding([.top, .bottom], 8)
+        .background(.backgroundPrimary)
+        .background(in: .rect(cornerRadius: 8))
+        .padding([.leading, .trailing], 16)
+        .padding(.bottom, 12)
+    }
+    
+    private var galleryButton: some View {
+        Button(action: {
+            showPhotosPicker = true
+        }, label: {
+            Image(systemName: "plus")
+                .resizable()
+                .frame(width: 18, height: 18)
+        })
+        .tint(.black)
+        .frame(width: 32, height: 32)
+        .photosPicker(isPresented: $showPhotosPicker, selection: $imagePickerModel.imageSelection, maxSelectionCount: 5)
+        
+    }
+    
+    func pickerImageCell(state: MultipleImagePickerModel.ImageStateModel) -> some View {
+        pickerImageView(state: state.state)
+            .clipShape(.rect(cornerRadius: 8))
+            .overlay(alignment: .topTrailing) {
+                Button(action: {
+                    imagePickerModel.removeImage(id: state.id)
+                }, label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .tint(.black)
+                        .frame(width: 6, height: 6)
+                        .frame(width: 16, height: 16)
+                        .background(.white)
+                        .clipShape(Circle())
+                        .overlay {
+                            Circle()
+                                .stroke(.black, lineWidth: 2)
+                        }
+                })
+                .offset(x: 4, y: -4)
+            }
+            .frame(width: 44, height: 44)
+    }
+    
+    
+    @ViewBuilder
+    func pickerImageView(state: MultipleImagePickerModel.ImageState) -> some View {
+        switch state {
+        case .empty:
+            Image(.workspaceBallon)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 48, height: 48)
+                .offset(y: 5)
+        case .loading:
+            ProgressView()
+        case .success(let image):
+            image.resizable()
+        case .failure:
+            Image(systemName: "exclamationmark.triangle.fill")
+                .foregroundStyle(.white)
+        }
+    }
+    
+    var chattingTextField: some View {
+        CustomTextView(text: $text, textViewHeight: $textViewHeight)
+            .frame(height: textViewHeight)
+    }
+    
+    var sendButton: some View {
+        Button(action: {
+            print(imagePickerModel.imageData)
+        }, label: {
+            Image(.send)
+                .resizable()
+                .frame(width: 26, height: 26)
+        })
+        .frame(width: 32, height: 32)
+    }
+}
+
+#Preview {
+    ChattingBarView(text: .constant(""), imagePickerModel: .init(maxSize: 44, imageData: []))
+}
