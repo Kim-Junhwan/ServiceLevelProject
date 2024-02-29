49f2e2be1b08fd5844d8230ab97a4d0ccb4e1170
Kim junhwan <tazanboy1@naver.com>
2024-02-29 18:34:25 +0900

    [feat] 코입샵 View 구현

---
+++ ServiceLevelProject/Presentation/Profile/Shop/CoinShopView.swift
@@ -0,0 +1,83 @@
+//
+//  CoinShopView.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/19.
+//
+
+import SwiftUI
+
+struct CoinShopView: View {
+    
+    @StateObject var viewModel: CoinShopViewModel = SharedAssembler.shared.resolve(CoinShopViewModel.self)
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(.white)
+                .frame(height: 44)
+                .overlay {
+                    HStack {
+                        Text("🌱 현재 보유한 코인")
+                            .font(CustomFont.bodyBold.font)
+                        Text("\(viewModel.state.hasCoin)개")
+                            .font(CustomFont.bodyBold.font)
+                            .foregroundStyle(.brandGreen)
+                        Spacer()
+                        Text("코인이란?")
+                            .font(CustomFont.caption.font)
+                            .foregroundStyle(.textSecondary)
+                    }
+                    .padding([.leading, .trailing], 15)
+                }
+            LazyVStack(spacing: 0) {
+                ForEach(viewModel.state.coinList) { coin in
+                    CoinCell(coin: coin) {
+                        viewModel.trigger(.tapBuyButton(coin))
+                    }
+                }
+            }
+            .clipShape(.rect(cornerRadius: 8))
+            Spacer()
+        }
+        .padding(24)
+        .background(.backgroundPrimary)
+        .underlineNavigationBar(title: "코인샵")
+        .toolbarBackground(.white, for: .navigationBar)
+        .onAppear {
+            viewModel.trigger(.appearView)
+        }
+    }
+    
+    
+}
+
+struct CoinCell: View {
+    let coin: CoinItemModel
+    let action: () -> Void
+    
+    var body: some View {
+        HStack {
+            Text("🌱 \(coin.name)")
+                .font(CustomFont.bodyBold.font)
+            Spacer()
+            Button(action: {
+                action()
+            }, label: {
+                Text("￦\(coin.price)")
+                    .font(CustomFont.title2.font)
+                    .foregroundStyle(.white)
+                    .frame(width: 74, height: 28)
+                    .background(.brandGreen)
+                    .background(in: .rect(cornerRadius: 8))
+            })
+        }
+        .padding([.leading, .trailing], 15)
+        .frame(height: 44)
+        .background(.white)
+    }
+}
+
+#Preview {
+    CoinShopView()
+}
