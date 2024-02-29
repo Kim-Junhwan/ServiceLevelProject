cc167f4abcbcc8e8ba7b6487857766fef347abbe
Kim junhwan <tazanboy1@naver.com>
2024-02-29 18:35:04 +0900

    [feat] 코인샵 ViewModel 구현

---
+++ ServiceLevelProject/Presentation/Profile/Shop/CoinShopViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  CoinShopViewModel.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/29.
+//
+
+import Foundation
+import Combine
+
+struct CoinItemModel: Identifiable {
+    let id: UUID = .init()
+    let name: String
+    let price: String
+}
+
+class CoinShopViewModel: ViewModel, ObservableObject {
+    enum CoinShopInput {
+        case appearView
+        case tapBuyButton(CoinItemModel)
+    }
+    
+    struct CoinShopState {
+        var coinList: [CoinItemModel] = []
+        var hasCoin: Int = 0
+    }
+    
+    @Published var state: CoinShopState = .init()
+    let profileRepository: ProfileRepository
+    let appState: AppState
+    private var cancellable = Set<AnyCancellable>()
+    
+    init(appState: AppState, profileRepository: ProfileRepository) {
+        self.profileRepository = profileRepository
+        self.appState = appState
+        appStateBind()
+    }
+    
+    private func appStateBind() {
+        appState.$userData.sink { userData in
+            self.state.hasCoin = userData.sesacCoin
+        }
+        .store(in: &cancellable)
+    }
+    
+    func trigger(_ input: CoinShopInput) {
+        switch input {
+        case .appearView:
+            fetchCoinList()
+        case .tapBuyButton(let coin):
+            buyCoin(coin: coin)
+        }
+    }
+    
+    func fetchCoinList() {
+        Task {
+            do {
+                let cointList = try await profileRepository.fetchCoinList()
+                DispatchQueue.main.async {
+                    self.state.coinList = cointList.map { .init(name: $0.item, price: $0.amount) }
+                }
+            }
+        }
+    }
+    
+    func buyCoin(coin: CoinItemModel) {
+        print(coin)
+    }
+}
