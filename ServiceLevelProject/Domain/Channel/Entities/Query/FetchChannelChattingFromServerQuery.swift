baeb56aefcf480a2d6c10d1d8879771956beb041
Kim junhwan <tazanboy1@naver.com>
2024-02-29 17:27:50 +0900

    [feat] 채널 채팅 가지고 오는 쿼리문 구현

---
+++ ServiceLevelProject/Domain/Channel/Entities/Query/FetchChannelChattingFromServerQuery.swift
@@ -0,0 +1,14 @@
+//
+//  FetchChannelChattingFromServerQuery.swift
+//  ServiceLevelProject
+//
+//  Created by JunHwan Kim on 2024/02/29.
+//
+
+import Foundation
+
+struct FetchChannelChattingFromServerQuery {
+    let workspaceId: Int
+    let channelName: String
+    let cursorDate: Date
+}
