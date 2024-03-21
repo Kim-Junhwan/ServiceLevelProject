88240f4c7613d25d747a647f24d2f8dd4d7ba31f
Kim junhwan <tazanboy1@naver.com>
2024-03-22 02:33:46 +0900

    [feat] SendDMChattingUsecase 채팅 저장기능 추가

--- ServiceLevelProject/Domain/DirectMessage/Usecase/SendDMChattingUsecase.swift
+++ ServiceLevelProject/Domain/DirectMessage/Usecase/SendDMChattingUsecase.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 protocol SendDMChattingUsecase {
-    func excute(roomId: Int, workspaceId: Int, content: String?, files: [Data]) async throws -> ChattingMessageModel
+    func excute(roomId: Int, workspaceId: Int, content: String?, files: [Data]) async throws -> DMChatting
+    func saveChatting(roomId: Int, workspaceId: Int, chatting: [DMChatting]) async throws
 }
 
 final class DefaultSendDMChattingUsecase {
@@ -22,9 +23,13 @@ final class DefaultSendDMChattingUsecase {
 }
 
 extension DefaultSendDMChattingUsecase: SendDMChattingUsecase {
-    func excute(roomId: Int, workspaceId: Int, content: String?, files: [Data]) async throws -> ChattingMessageModel {
+    func saveChatting(roomId: Int, workspaceId: Int, chatting: [DMChatting]) async throws {
+        try await chattingRepository.saveDMChatting(.init(workspaceId: workspaceId, roomId: roomId, chats: chatting))
+    }
+    
+    func excute(roomId: Int, workspaceId: Int, content: String?, files: [Data]) async throws -> DMChatting {
         let sendChatting = try await dmRepository.postDMChatting(.init(roomId: roomId, workspaceId: workspaceId, content: content, files: files))
         try await chattingRepository.saveDMChatting(.init(workspaceId: workspaceId, roomId: roomId, chats: [sendChatting]))
-        return .init(chatId: sendChatting.dmId, content: sendChatting.content, createdAt: sendChatting.createdAt, files: sendChatting.files, user: .init(userThumnail: sendChatting.user))
+        return sendChatting
     }
 }
